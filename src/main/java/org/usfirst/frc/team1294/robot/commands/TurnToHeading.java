1b855a665bfdb320a3cd77a0effc9d1e11b30845
TonyStew <bino4444@gmail.com>
2017-01-18 20:48:36 -0800

    First TurnToHeading build

--- src/main/java/org/usfirst/frc/team1294/robot/commands/TurnToHeading.java
+++ src/main/java/org/usfirst/frc/team1294/robot/commands/TurnToHeading.java
@@ -1,28 +1,55 @@
 package org.usfirst.frc.team1294.robot.commands;
 
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.PIDCommand;
 import org.usfirst.frc.team1294.robot.Robot;
+import org.usfirst.frc.team1294.robot.RobotMap;
+import org.usfirst.frc.team1294.robot.subsystems.DriveSubsystem;
 
 /**
  * A command that turns the robot to a particular heading relative to the field.
  */
-public class TurnToHeading extends Command {
+public class TurnToHeading extends PIDCommand {
 
     private final double heading;
 
+    private static final double kP = 0.03;
+    private static final double kI = 0.00;
+    private static final double kD = 0.00;
+    private static final double kF = 0.00;
+
+    private final double kToleranceDegrees = 2.0f;
+
     public TurnToHeading(double heading) {
+        super(kP, kI, kD);
         requires(Robot.driveSubsystem);
         this.heading = heading;
     }
 
     @Override
     protected void execute() {
-        super.execute();
-        // TODO: implement this command
+        this.getPIDController().setInputRange(-180.0f,  180.0f);
+        this.getPIDController().setOutputRange(-1.0, 1.0);
+        this.getPIDController().setAbsoluteTolerance(kToleranceDegrees);
+        this.getPIDController().setContinuous(true);
     }
 
     @Override
     protected boolean isFinished() {
-        return false;
+        return this.getPIDController().onTarget();
+    }
+
+
+    @Override
+    protected double returnPIDInput() {
+        return Robot.driveSubsystem.getAngle();
+    }
+
+    @Override
+    protected void usePIDOutput(double output) {
+        Robot.driveSubsystem.mecanumDrive(0, 0, output, 0);
     }
 }
