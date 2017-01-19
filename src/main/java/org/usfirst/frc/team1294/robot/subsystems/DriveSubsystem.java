25c5d1b3281bdd266ee3f0c110b82f5ef589a612
Alyssa Giedd <alyssagieddrobotics@gmail.com>
2017-01-19 07:23:51 -0800

    Changed RampRate to a constant
    Changed RampRate to a constant in DriveSubsystem

--- src/main/java/org/usfirst/frc/team1294/robot/subsystems/DriveSubsystem.java
+++ src/main/java/org/usfirst/frc/team1294/robot/subsystems/DriveSubsystem.java
@@ -19,6 +19,7 @@ public class DriveSubsystem extends Subsystem {
   public final CANTalon rightRearTalon;
   private final RobotDrive robotDrive;
   private static AHRS navX;
+  private static double rampRate;
 
   public DriveSubsystem() {
     super("DriveSubsystem");
@@ -29,11 +30,12 @@ public class DriveSubsystem extends Subsystem {
     rightRearTalon = new CANTalon(RobotMap.DRIVEBASE_RIGHT_REAR_TALON);
     robotDrive = new RobotDrive(leftFrontTalon, leftRearTalon, rightFrontTalon, rightRearTalon);
     robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
+    rampRate = 12.0;
     navX = new AHRS(SerialPort.Port.kMXP);
-    leftFrontTalon.setVoltageRampRate (12.0);
-    rightFrontTalon.setVoltageRampRate (12.0);
-    leftRearTalon.setVoltageRampRate (12.0);
-    rightRearTalon.setVoltageRampRate (12.0);
+    leftFrontTalon.setVoltageRampRate (rampRate);
+    rightFrontTalon.setVoltageRampRate (rampRate);
+    leftRearTalon.setVoltageRampRate (rampRate);
+    rightRearTalon.setVoltageRampRate (rampRate);
   }
 
   @Override
