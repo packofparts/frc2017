1a31311a3a8b8a6d2f5020aa4d005bfde3e9733d
John R Lewis <jxlewis@godaddy.com>
2017-01-21 15:20:06 -0800

    tweaks

--- src/main/java/org/usfirst/frc/team1294/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team1294/robot/Robot.java
@@ -23,113 +23,115 @@ import org.usfirst.frc.team1294.robot.subsystems.FuelSubsystem;
  */
 public class Robot extends IterativeRobot {
 
-	public static OI oi;
-	public static DriveSubsystem driveSubsystem;
-	public static CameraSubsystem cameraSubsystem;
-    public static ClimbingSubsystem climbingSubsystem;
-    public static FuelSubsystem fuelSubsystem;
+  public static OI oi;
+  public static DriveSubsystem driveSubsystem;
+  public static CameraSubsystem cameraSubsystem;
+  public static ClimbingSubsystem climbingSubsystem;
+  public static FuelSubsystem fuelSubsystem;
 
 
-	Command autonomousCommand;
-	SendableChooser<Command> chooser = new SendableChooser<>();
+  Command autonomousCommand;
+  SendableChooser<Command> chooser = new SendableChooser<>();
 
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
-	@Override
-	public void robotInit() {
-		// THESE MUST BE INITIALIZED FIRST
-		oi = new OI();
-		driveSubsystem = new DriveSubsystem();
-		cameraSubsystem = new CameraSubsystem();
-        climbingSubsystem = new ClimbingSubsystem();
-        fuelSubsystem = new FuelSubsystem();
+  /**
+   * This function is run when the robot is first started up and should be
+   * used for any initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // THESE MUST BE INITIALIZED FIRST
 
-		chooser.addDefault("Auto Gear Center", new AutoGearCenter());
-		chooser.addObject("Auto Gear Left", new AutoGearLeft());
-		chooser.addObject("Auto Gear Right", new AutoGearRight());
-		SmartDashboard.putData("Auto mode", chooser);
+    driveSubsystem = new DriveSubsystem();
+    cameraSubsystem = new CameraSubsystem();
+    climbingSubsystem = new ClimbingSubsystem();
+    fuelSubsystem = new FuelSubsystem();
 
-		SmartDashboard.putData(new ResetGyroCommand());
-		SmartDashboard.putData(new DriveBaseBreakInCommand());
-		SmartDashboard.putData(new DoGearCameraImageProcessingCommand());
-		SmartDashboard.putData(new DeliverGearCommand());
-	}
+    oi = new OI();
 
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
-	@Override
-	public void disabledInit() {
+    chooser.addDefault("Auto Gear Center", new AutoGearCenter());
+    chooser.addObject("Auto Gear Left", new AutoGearLeft());
+    chooser.addObject("Auto Gear Right", new AutoGearRight());
+    SmartDashboard.putData("Auto mode", chooser);
 
-	}
+    SmartDashboard.putData(new ResetGyroCommand());
+    SmartDashboard.putData(new DriveBaseBreakInCommand());
+    SmartDashboard.putData(new DoGearCameraImageProcessingCommand());
+    SmartDashboard.putData(new DeliverGearCommand());
+  }
 
-	@Override
-	public void disabledPeriodic() {
-		Scheduler.getInstance().run();
-	}
+  /**
+   * This function is called once each time the robot enters Disabled mode.
+   * You can use it to reset any subsystem information you want to clear when
+   * the robot is disabled.
+   */
+  @Override
+  public void disabledInit() {
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
-	 */
-	@Override
-	public void autonomousInit() {
-		autonomousCommand = chooser.getSelected();
+  }
+
+  @Override
+  public void disabledPeriodic() {
+    Scheduler.getInstance().run();
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable
+   * chooser code works with the Java SmartDashboard. If you prefer the
+   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+   * getString code to get the auto name from the text box below the Gyro
+   * <p>
+   * You can add additional auto modes by adding additional commands to the
+   * chooser code above (like the commented example) or additional comparisons
+   * to the switch structure below with additional strings & commands.
+   */
+  @Override
+  public void autonomousInit() {
+    autonomousCommand = chooser.getSelected();
 
 		/*
-		 * String autoSelected = SmartDashboard.getString("Auto Selector",
+     * String autoSelected = SmartDashboard.getString("Auto Selector",
 		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
 		 * = new MyAutoCommand(); break; case "Default Auto": default:
 		 * autonomousCommand = new ExampleCommand(); break; }
 		 */
 
-		// schedule the autonomous command (example)
-		if (autonomousCommand != null)
-			autonomousCommand.start();
-	}
+    // schedule the autonomous command (example)
+    if (autonomousCommand != null)
+      autonomousCommand.start();
+  }
 
-	/**
-	 * This function is called periodically during autonomous
-	 */
-	@Override
-	public void autonomousPeriodic() {
-		Scheduler.getInstance().run();
-	}
+  /**
+   * This function is called periodically during autonomous
+   */
+  @Override
+  public void autonomousPeriodic() {
+    Scheduler.getInstance().run();
+  }
 
-	@Override
-	public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
-		if (autonomousCommand != null)
-			autonomousCommand.cancel();
-	}
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (autonomousCommand != null)
+      autonomousCommand.cancel();
+  }
 
-	/**
-	 * This function is called periodically during operator control
-	 */
-	@Override
-	public void teleopPeriodic() {
-		Scheduler.getInstance().run();
-	}
+  /**
+   * This function is called periodically during operator control
+   */
+  @Override
+  public void teleopPeriodic() {
+    Scheduler.getInstance().run();
+  }
 
-	/**
-	 * This function is called periodically during test mode
-	 */
-	@Override
-	public void testPeriodic() {
-		LiveWindow.run();
-	}
+  /**
+   * This function is called periodically during test mode
+   */
+  @Override
+  public void testPeriodic() {
+    LiveWindow.run();
+  }
 }
