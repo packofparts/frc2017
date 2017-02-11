6843941043cca707960592ebdae3e58af4b55820
Alyssa Giedd <alyssagieddrobotics@gmail.com>
2017-02-11 12:11:13 -0800

    Added the shooter command

--- src/main/java/org/usfirst/frc/team1294/robot/commands/ShooterCommand.java
+++ src/main/java/org/usfirst/frc/team1294/robot/commands/ShooterCommand.java
@@ -1,7 +1,28 @@
 package org.usfirst.frc.team1294.robot.commands;
 
+import org.usfirst.frc.team1294.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
 /**
  * Created by root on 2/11/17.
  */
-public class ShooterCommand {
+public class ShooterCommand extends Command{
+	@Override
+    protected void execute() {
+		super.execute();
+		Robot.fuelSubsystem.setShooterMotorBVoltageSpeed(10);
+		Robot.fuelSubsystem.setShooterMotorAVoltageSpeed(8);
+    }
+	protected void end() {
+		super.execute();
+		Robot.fuelSubsystem.setShooterMotorBVoltageSpeed(0);
+		Robot.fuelSubsystem.setShooterMotorAVoltageSpeed(0);
+    }
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 }
