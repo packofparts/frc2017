a6b98dc0050edcce8a30996b327e40b097de1db0
Alyssa Giedd <alyssagieddrobotics@gmail.com>
2017-02-19 13:59:40 -0800

    Added Commands for the right side
    Added command to handle the right side in case of the opposite alliance
    (see field diagram)

--- src/main/java/org/usfirst/frc/team1294/robot/commands/MoveFromStartToLiftLeftWithShooter.java
+++ src/main/java/org/usfirst/frc/team1294/robot/commands/MoveFromStartToLiftLeftWithShooter.java
@@ -6,12 +6,12 @@ package org.usfirst.frc.team1294.robot.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
-public class MoveFromStartToLiftRightWithShooter extends CommandGroup{
+public class MoveFromStartToLiftLeftWithShooter extends CommandGroup{
     private static final double TIME_TO_WAIT = 3.0;
-    private static final double HEADING_TO_TURN = 45;
+    private static final double HEADING_TO_TURN = -45;
     //do the math for this distance later
     private static final double DISTANCE_TO_DRIVE = -1.5;
-    public MoveFromStartToLiftRightWithShooter() {
+    public MoveFromStartToLiftLeftWithShooter() {
     	addSequential(new MoveFromStartToLiftRight());
         addSequential(new WaitCommand(TIME_TO_WAIT));
         addSequential(new DriveStraightCommand(DISTANCE_TO_DRIVE));
