be6a22390512c547cca15e324933cfa44d260b28
Alyssa Giedd <alyssagieddrobotics@gmail.com>
2017-02-19 13:50:31 -0800

    Added correct addsequential commands
    Added new commands for auto, need to do some testing to determine the
    correct values

--- src/main/java/org/usfirst/frc/team1294/robot/commands/MoveFromStartToLiftRightWithShooter.java
+++ src/main/java/org/usfirst/frc/team1294/robot/commands/MoveFromStartToLiftRightWithShooter.java
@@ -1,19 +1,21 @@
+//**
+//Authored by Alyssa Giedd
+//**
 package org.usfirst.frc.team1294.robot.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class MoveFromStartToLiftRightWithShooter extends CommandGroup{
-	public static final double DISTANCE_TO_TRAVEL = 1.8;
-    public static final int HEADING_TO_FACE = -90;
     private static final double TIME_TO_WAIT = 3.0;
-    private static final double DISTANCE_TO_REVERSE = -0.5;
-    
+    private static final double HEADING_TO_TURN = 45;
+    //do the math for this distance later
+    private static final double DISTANCE_TO_DRIVE = 1.5;
     public MoveFromStartToLiftRightWithShooter() {
-    	addSequential(new DriveStraightCommand(DISTANCE_TO_TRAVEL));
-        addSequential(new TurnToHeading(HEADING_TO_FACE));
-        addSequential(new DeliverGearCommand());
+    	addSequential(new MoveFromStartToLiftRight());
         addSequential(new WaitCommand(TIME_TO_WAIT));
+        addSequential(new DriveStraightCommand(DISTANCE_TO_DRIVE));
+        addSequential(new TurnToHeading(HEADING_TO_TURN));
         addSequential(new ShooterCommand());
         addSequential(new FeederCommand());
         addSequential(new FeederCommand());
