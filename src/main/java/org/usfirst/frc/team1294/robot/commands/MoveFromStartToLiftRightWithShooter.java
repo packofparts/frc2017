f0772605bb5ac88d22478966252afedbdd19707b
Alyssa Giedd <alyssagieddrobotics@gmail.com>
2017-02-19 13:00:19 -0800

    Refactored to account for Right Gear
    Changed to right gear as opposed to center gear

--- src/main/java/org/usfirst/frc/team1294/robot/commands/MoveFromStartToLiftRightWithShooter.java
+++ src/main/java/org/usfirst/frc/team1294/robot/commands/MoveFromStartToLiftRightWithShooter.java
@@ -3,17 +3,17 @@ package org.usfirst.frc.team1294.robot.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
-public class MoveFromStartToLiftCenterWithShooter extends CommandGroup{
+public class MoveFromStartToLiftRightWithShooter extends CommandGroup{
 	public static final double DISTANCE_TO_TRAVEL = 1.8;
     public static final int HEADING_TO_FACE = -90;
     private static final double TIME_TO_WAIT = 3.0;
     private static final double DISTANCE_TO_REVERSE = -0.5;
     
-    public MoveFromStartToLiftCenterWithShooter() {
-        addSequential(new DriveStraightCommand(DISTANCE_TO_TRAVEL));
+    public MoveFromStartToLiftRightWithShooter() {
+    	addSequential(new DriveStraightCommand(DISTANCE_TO_TRAVEL));
+        addSequential(new TurnToHeading(HEADING_TO_FACE));
         addSequential(new DeliverGearCommand());
         addSequential(new WaitCommand(TIME_TO_WAIT));
-        addSequential(new DriveStraightCommand(DISTANCE_TO_REVERSE));
         addSequential(new ShooterCommand());
         addSequential(new FeederCommand());
         addSequential(new FeederCommand());
